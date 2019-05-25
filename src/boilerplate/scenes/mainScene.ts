e63336ee3af9e7b41c75c37b5c9dd3fe49408e0a
David Neilsen <petah.p@gmail.com>
2019-05-25 14:10:52 +1200

    Stuff

--- src/boilerplate/scenes/mainScene.ts
+++ src/boilerplate/scenes/mainScene.ts
@@ -1,23 +1,17 @@
-/**
- * @author       Digitsensitive <digit.sensitivee@gmail.com>
- * @copyright    2018 - 2019 digitsensitive
- * @license      Digitsensitive
- */
-
 export class MainScene extends Phaser.Scene {
-  private phaserSprite: Phaser.GameObjects.Sprite;
+    private phaserSprite: Phaser.GameObjects.Sprite;
 
-  constructor() {
-    super({
-      key: "MainScene"
-    });
-  }
+    constructor() {
+        super({
+            key: "MainScene",
+        });
+    }
 
-  preload(): void {
-    this.load.image("logo", "./src/boilerplate/assets/phaser.png");
-  }
+    public preload(): void {
+        this.load.image("logo", "./src/boilerplate/assets/phaser.png");
+    }
 
-  create(): void {
-    this.phaserSprite = this.add.sprite(400, 300, "logo");
-  }
+    public create(): void {
+        this.phaserSprite = this.add.sprite(400, 300, "logo");
+    }
 }
