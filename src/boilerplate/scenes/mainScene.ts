bfc29cc48b497ffc9b1968aa52e2c7dc8adc0c24
David Neilsen <petah.p@gmail.com>
2019-05-26 10:43:11 +1200

    load a tile map

--- src/boilerplate/scenes/mainScene.ts
+++ src/boilerplate/scenes/mainScene.ts
@@ -1,6 +1,9 @@
 export class MainScene extends Phaser.Scene {
     private phaserSprite: Phaser.GameObjects.Sprite;
 
+    map: Phaser.Tilemaps.Tilemap;
+    backgroundLayer: any;
+
     constructor() {
         super({
             key: "MainScene",
@@ -8,10 +11,13 @@ export class MainScene extends Phaser.Scene {
     }
 
     public preload(): void {
-        this.load.image("logo", "./src/boilerplate/assets/phaser.png");
+        this.load.image("tiles", "./src/boilerplate/assets/Tileset_Large.png");
+        this.load.tilemapTiledJSON("Tile Layer 1", "./src/boilerplate/tilemaps/test.json");
     }
 
     public create(): void {
-        this.phaserSprite = this.add.sprite(400, 300, "logo");
+        this.map = this.add.tilemap("Tile Layer 1");
+        var tileset = this.map.addTilesetImage("test", "tiles");
+        this.backgroundLayer = this.map.createStaticLayer("Tile Layer 1", tileset);
     }
 }
